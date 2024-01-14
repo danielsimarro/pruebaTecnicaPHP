782f910a204b8fb7ac0ac6ccfde2a7e41c23f5fa
danielsimarro <danisimarroreigada@gmail.com>
2024-01-14 16:07:19 +0100

    Creation of the project and basic structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UFT-8">
+    <title>Prueba Técnica</title>
+    <script>
+    </script>
+</head>
+
+<body>
+    <h1>Welcome to ChuckNorris</h1>
+</body>
+
+</html>
